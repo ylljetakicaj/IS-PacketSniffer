2fa4a112b4461e4f010f646b743aa9e256cd7e8d
ylljetakicaj <118022552+ylljetakicaj@users.noreply.github.com>
2024-05-16 10:41:00 +0200

    Capturing Traffic

--- sniffer_demo.py
+++ sniffer_demo.py
@@ -2,6 +2,15 @@ import socket
 import struct
 import textwrap
 
+def main():
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+    while True:
+        raw_data, addr = conn.recvfrom(65536)
+        dest_mac, str_mac, eth_proto, data = ethernet_frame(raw_data)
+        print('\nEthernet frame:')
+        print('Destination: {}, Source: {}, Protocol: {}.format(dest_mac, src_mac, eth_proto))
+              
 def ethernnet_frame(data):
     dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14]
     return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[:14]
