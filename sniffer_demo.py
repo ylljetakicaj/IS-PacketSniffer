93ffede34000a188e8dd531b64eee5964d8ed268
ylljetakicaj <118022552+ylljetakicaj@users.noreply.github.com>
2024-05-16 10:35:12 +0200

    Formatting MAC Address

--- sniffer_demo.py
+++ sniffer_demo.py
@@ -5,3 +5,7 @@ import textwrap
 def ethernnet_frame(data):
     dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14]
     return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[:14]
+
+def get_mac_addr(bytes_addr):
+    bytes_str = map('{02x}'.format, bytes_addr)
+    return ':'.join(bytes_str).upper()
