b27efc91958d8600b4bec7e21a7d5b587af88c01
YllkaNishevci <119699302+YllkaNishevci@users.noreply.github.com>
2024-05-19 22:01:24 +0200

    Update sniffer_gui.py

--- gui/sniffer_gui.py
+++ gui/sniffer_gui.py
@@ -5,3 +5,19 @@ import scapy.all as scapy
 from datetime import datetime
 from utils.packet_utils import packet_callback
 
+class SnifferGUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Packet Sniffer")
+        
+        self.text_area = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=150, height=30)
+        self.text_area.pack()
+
+        self.start_button = tk.Button(root, text="Start Sniffing", command=self.start_sniffing)
+        self.start_button.pack()
+
+        self.stop_button = tk.Button(root, text="Stop Sniffing", command=self.stop_sniffing, state=tk.DISABLED)
+        self.stop_button.pack()
+
+        self.sniffing = False
+
