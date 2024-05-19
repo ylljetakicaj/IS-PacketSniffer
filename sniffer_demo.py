f137cdecc91bc58914c18e4be10a5a32a277509f
Vesa Zhitia <118364175+vesax@users.noreply.github.com>
2024-05-19 12:57:09 +0200

    Unpack IPv4 packets

--- sniffer_demo.py
+++ sniffer_demo.py
@@ -18,3 +18,11 @@ def ethernnet_frame(data):
 def get_mac_addr(bytes_addr):
     bytes_str = map('{02x}'.format, bytes_addr)
     return ':'.join(bytes_str).upper()
+
+#unpack IPv4 packets
+def ipv4_packet(data):
+    version_header_length = data[0]
+    version = version_header_length >> 4 #bit shift it by 4 to the right
+    header_length = (version_header_length & 15) * 4 #compare 2 bytes
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    return version, header_length, ttl, proto, ipv4(src), ipv4(target), data[header_length:]
