5d81229e7eb76b0b2e8d3abc69084d3b05a1984a
Vesa Zhitia <118364175+vesax@users.noreply.github.com>
2024-05-19 22:29:03 +0200

    Update sniffer_gui.py

--- gui/sniffer_gui.py
+++ gui/sniffer_gui.py
@@ -35,3 +35,47 @@ class SnifferGUI:
 
     def sniff_packets(self):
         scapy.sniff(prn=self.display_packet, store=0, stop_filter=lambda x: not self.sniffing)
+
+    def display_packet(self, packet):
+        if scapy.Ether in packet:
+            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            
+            # Ethernet details
+            eth_src = packet[scapy.Ether].src
+            eth_dst = packet[scapy.Ether].dst
+            eth_proto = packet[scapy.Ether].type
+            
+            # IPv4 details
+            if scapy.IP in packet:
+                ip_src = packet[scapy.IP].src
+                ip_dst = packet[scapy.IP].dst
+                ip_proto = packet[scapy.IP].proto
+                ip_version = packet[scapy.IP].version
+                ip_ihl = packet[scapy.IP].ihl
+                ip_ttl = packet[scapy.IP].ttl
+            else:
+                ip_src = ip_dst = ip_proto = ip_version = ip_ihl = ip_ttl = None
+            
+            # TCP details
+            if scapy.TCP in packet:
+                tcp_sport = packet[scapy.TCP].sport
+                tcp_dport = packet[scapy.TCP].dport
+                tcp_seq = packet[scapy.TCP].seq
+                tcp_flags = packet[scapy.TCP].flags
+            else:
+                tcp_sport = tcp_dport = tcp_seq = tcp_flags = None
+            
+            log_message = (
+                f"{timestamp} - Ethernet: {eth_src} -> {eth_dst} [Protocol: {eth_proto}], "
+                f"IPv4: {ip_src} -> {ip_dst} [Protocol: {ip_proto}, Version: {ip_version}, "
+                f"Header Length: {ip_ihl}, TTL: {ip_ttl}], "
+                f"TCP: {tcp_sport} -> {tcp_dport} [Seq: {tcp_seq}, Flags: {tcp_flags}]\n"
+            )
+            self.text_area.insert(tk.END, log_message)
+            self.text_area.yview(tk.END)
+            packet_callback(packet)  # Log to file as well
+
+def start_gui():
+    root = tk.Tk()
+    app = SnifferGUI(root)
+    root.mainloop()
